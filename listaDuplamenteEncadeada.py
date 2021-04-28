afaa46de890857178da8857399fc39444769e307
gabrielgarciac <74276344+gabrielgarciac@users.noreply.github.com>
2021-04-27 21:30:30 -0300

    Update listaDuplamenteEncadeada.py

--- listaDuplamenteEncadeada.py
+++ listaDuplamenteEncadeada.py
@@ -1,36 +1,94 @@
 class Node:
 
-	# Funnção inicializadora
+	#Construtor para criar um novo nó
 	def __init__(self, data):
-		self.data = data 
-		self.next = None 
+		self.data = data
+		self.next = None
+		self.prev = None
 
+#Classe para criar uma lista duplamente encadeada
+class DoublyLinkedList:
 
-#A classe ListaEncadeada contem um objeto Node
-class ListaEncadeada:
-
-	#Função que inicializa a "cabeça" da lista
+	#Construtor para lista duplamente encadeada vazia
 	def __init__(self):
-		self.cabeca = None
+		self.head = None
 
-	#Função para realizar o print da lista
-	def printList(self):
-		temp = self.cabeca
-		while (temp):
-			print (temp.data)
-			temp = temp.next
+	def push(self, new_data):
+
+		#Aloca um nó e adiciona dados a ele
+		new_node = Node(new_data)
+
+		new_node.next = self.head
+
+		if self.head is not None:
+			self.head.prev = new_node
+
+		#Move a cabeça da lista para um novo nó
+		self.head = new_node
+
+	def insertAfter(self, prev_node, new_data):
+
+		#Checa se nó anterior está vazio
+		if prev_node is None:
+			print ("the given previous node cannot be NULL")
+			return
+
+		#Aloca um nó e adiciona dados a ele
+		new_node = Node(new_data)
+		
+		new_node.next = prev_node.next
+		
+		prev_node.next = new_node
+
+		new_node.prev = prev_node
+
+		if new_node.next:
+			new_node.next.prev = new_node
+
+	def append(self, new_data):
+
+		#Aloca um nó e adiciona dados a ele
+		new_node = Node(new_data)
+
+		#Esse novo nó será o ultimo, portanto o proximo é automaticamente nulo
+
+		if self.head is None:
+			self.head = new_node
+			return
+
+		last = self.head
+		while last.next:
+			last = last.next
+
+		last.next = new_node
+
+		new_node.prev = last
+
+		return
+
+	#Função para fazer o print da lista
+	def printList(self, node):
+
+		print ("\nTraversal in forward direction")
+		while node:
+			print (" % d" % (node.data)),
+			last = node
+			node = node.next
+
+		print ("\nTraversal in reverse direction")
+		while last:
+			print (" % d" % (last.data)),
+			last = last.prev
 
 
-if __name__=='__main__':
+#Começa com uma lista vazia
+llist = DoublyLinkedList()
+llist.append(6)
+llist.push(7)
+llist.push(1)
+llist.append(4)
 
-	#Começa com uma lista vazia
-	llist = ListaEncadeada()
+llist.insertAfter(llist.head.next, 8)
 
-	llist.cabeca = Node(1)
-	segundo = Node(2)
-	terceiro = Node(3)
-
-	llist.cabeca.next = segundo; # Linka o primeiro nó com o segundo
-	segundo.next = terceiro; # Linka o segundo nó com o  terceiro
-
-	llist.printList()
+print ("Created DLL is: "),
+llist.printList(llist.head)
